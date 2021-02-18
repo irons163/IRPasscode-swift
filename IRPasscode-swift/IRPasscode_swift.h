400ccec14ac448d96cd2037a301759a49ca8e583
Phil <phil@PhildeMac-mini.engenius.login>
2021-02-18 19:05:35 +0800

    First commit.

---
+++ IRPasscode-swift/IRPasscode_swift.h
@@ -0,0 +1,19 @@
+//
+//  IRPasscode_swift.h
+//  IRPasscode-swift
+//
+//  Created by Phil on 2020/9/30.
+//  Copyright © 2020 Phil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IRPasscode_swift.
+FOUNDATION_EXPORT double IRPasscode_swiftVersionNumber;
+
+//! Project version string for IRPasscode_swift.
+FOUNDATION_EXPORT const unsigned char IRPasscode_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IRPasscode_swift/PublicHeader.h>
+
+
